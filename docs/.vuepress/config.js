f707da38421c75d0abc1cfdd4a42dc6ce6d8d267
demigods <a13752365641@126.com>
2019-05-16 09:28:00 +0800

    添加基础文件

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,61 @@
+module.exports = {
+  title: '陨落的半神',
+  description: '个人博客及作品展示',
+  // 注入到当前页面的 HTML <head> 中的标签
+  head: [
+    ['link', {
+      rel: 'icon',
+      href: '/favicon.ico'
+    }],
+  ],
+  base: '/',
+  markdown: {
+    lineNumbers: true // 代码块显示行号
+  },
+  themeConfig: {
+    sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
+    lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
+    // 导航
+    nav: [{
+        text: 'HTML',
+        link: '/HTML/'
+      },
+      {
+        text: 'CSS',
+        link: '/CSS/'
+      },
+      {
+        text: 'JavaScript',
+        link: '/JavaScript/'
+      },
+      {
+        text: 'About',
+        link: '/About/'
+      },
+      {
+        text: 'GitHub',
+        link: 'https://github.com/SonDemigods'
+      }
+    ],
+    // 侧边栏
+    sidebar: {
+      '/HTML/': [
+        ''
+      ],
+      '/CSS/': [
+        ''
+      ],
+      '/JavaScript/': [
+        '',
+        'ajax'
+      ],
+      '/About/': [
+        ''
+      ],
+      // fallback
+      '/': [
+        ''
+      ]
+    }
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroImage: /banner.jpg
+actionText: 关于我
+actionLink: /About/
+# features:
+#   - title: HTML
+#     details: HTML
+#   - title: CSS
+#     details: CSS
+#   - title: JavaScript
+#     details: JavaScript
+footer: Copyright © 2019 demigods
+---
